cea1f675d30be80e65607be8387c38b5938f34ec
LucasTKP <101598532+LucasTKP@users.noreply.github.com>
2023-03-01 19:28:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
-const cors = require('cors');
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 app.use(express.json())
 app.use(routes)
 
